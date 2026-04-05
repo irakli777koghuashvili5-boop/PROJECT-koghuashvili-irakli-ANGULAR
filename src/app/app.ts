0d7a138723bcdf5e33c82b3db2430c47284087f6
Default <irakli777koghuashvili5@gmail.com>
2026-04-05 19:20:26 +0400

    pr 1 st

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
+import { Hotels } from './hotels/hotels';
+import { BookedRooms } from './booked-rooms/booked-rooms';
+import { Rooms } from './rooms/rooms';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Header,Footer,Hotels,BookedRooms,Rooms,Home],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/booked-rooms/booked-rooms.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booked-rooms',
+  imports: [],
+  templateUrl: './booked-rooms.html',
+  styleUrl: './booked-rooms.scss',
+})
+export class BookedRooms {}
