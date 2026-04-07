fdcfa7d59eb7c252112f8adc1f7b93b721a0e2dc
Default <irakli777koghuashvili5@gmail.com>
2026-04-07 15:30:24 +0400

    hw 6-3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
-import { Routes } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet, Routes } from '@angular/router';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
+import { Hotels } from './hotels/hotels';
+import { BookedRooms } from './booked-rooms/booked-rooms';
+import { Rooms } from './rooms/rooms';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: Home,
+  },
+  {
+    path: 'hotels',
+    component: Hotels,
+  },
+  {
+    path: 'booked-rooms',
+    component: BookedRooms,
+  },
+  {
+    path: 'rooms',
+    component: Rooms,
+  },
+];
